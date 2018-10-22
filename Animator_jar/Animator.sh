e0eb527c1e6db3d9a5a943ca48e33b531587d7d9
yasz24 <yash17shetty@gmail.com>
2018-10-21 23:08:44 -0700

    added jar

--- Animator_jar/Animator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-####### testing commenting ##
-java -jar Animator.jar -if ../../../buildings.txt -iv interactive -speed 200
--- out/artifacts/Animator_jar/execute_jar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar Animator.jar -if big-bang-big-crunch.txt -iv interact_slider
